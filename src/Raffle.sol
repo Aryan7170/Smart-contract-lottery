5fa6f5ddfe48cd2acafcd32835162bce711a8184
Aryan7170 <aryanduhoon7170@gmail.com>
2025-05-28 20:11:08 +0530

    Fix: add dependency install step to GitHub Actions workflow

--- src/Raffle.sol
+++ src/Raffle.sol
@@ -141,7 +141,6 @@ contract Raffle is VRFConsumerBaseV2Plus, AutomationCompatibleInterface {
      * calls to send the money to the random winner.
      */
     function fulfillRandomWords(uint256, /* requestId */ uint256[] calldata randomWords) internal override {
-        
         uint256 indexOfWinner = randomWords[0] % s_players.length;
         address payable recentWinner = s_players[indexOfWinner];
         s_recentWinner = recentWinner;
